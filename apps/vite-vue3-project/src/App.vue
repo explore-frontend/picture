9f2e0fc41cae504ef1a857fa10617eaf950f3e06
Amorites <751809522@qq.com>
2022-10-21 15:25:33 +0800

    refactor: monoreop

---
+++ apps/vite-vue3-project/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Picture from '@kwai-explore/picture.vue';
+import examplePic from './assets/example.jpg?preset=modern';
+</script>
+
+<template>
+  <Picture :src="examplePic"></Picture>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vuehover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ apps/vite-vue3-project/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import imagePresets, { formatPreset } from 'vite-plugin-image-presets';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    imagePresets({
+      modern: formatPreset({
+        formats: {
+          avif: {},
+          webp: {},
+          original: {},
+        },
+        loading: 'lazy',
+      }),
+    }),
+  ],
+});
