427b49a7e65fead139b40996a98c9ea7ce22945c
Amorites <751809522@qq.com>
2022-10-24 03:13:24 +0800

    test: add basic test

---
+++ apps/vite-vue2-project/tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page, browserName }) => {
+    page.on('request', (request) => {
+        console.log('>>', request.method(), request.url());
+        const url = request.url();
+        if (url.includes('Gardens')) {
+            if (browserName === 'chromium') {
+                expect(url.endsWith('avif')).toBeTruthy();
+                // 这里以后有自动升级系统的话结果可能就会变了，不知道应该怎么限制一下
+            } else if (browserName === 'webkit') {
+                // 判断所有请求都是 webp 应该也没问题
+                expect(url.endsWith('webp')).toBeTruthy();
+            }
+        }
+    });
+    await page.goto('http://localhost:4173', {
+        waitUntil: 'networkidle',
+    });
+});
--- apps/vite-vue2-project/vite.config.mts
+++ apps/vite-vue2-project/vite.config.mts
@@ -30,7 +30,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
-            vue: require.resolve('vue'),
+            // vue: require.resolve('vue'),
         },
     },
 });
